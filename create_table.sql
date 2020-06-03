fcbc95748b83405cb600e5c4c5f18dbecca29cf4
busy0312 <abbymacbook@Abbys-MacBook-Pro.local>
2020-06-03 03:58:57 -0500

    SQL is done

---
+++ create_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Austin_animals (
+	"Animal_ID" CHAR(15) PRIMARY KEY NOT NULL,
+	"Name" VARCHAR,
+    "DateTime_Outcome" Date,
+    "Date_of_Birth" Date,
+    "Outcome_Type" VARCHAR,
+    "Animal_Type" VARCHAR NOT NULL,
+    "Sex_upon_Outcome" VARCHAR,
+   "Age_upon_Outcome" VARCHAR,
+    "Breed" VARCHAR NOT NULL,
+    "Color"VARCHAR NOT NULL,
+    "DateTime_Intake" Date,
+    "Intake_Type" VARCHAR,
+    "Age_upon_Intake" VARCHAR
+);
