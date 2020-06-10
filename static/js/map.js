5b4e947a6c01b194e960e37ff98028ef71c55b8b
busy0312 <abbymacbook@Abbys-MacBook-Pro.local>
2020-06-09 21:52:25 -0500

    map

--- static/js/map.js
+++ static/js/map.js
@@ -1,30 +1,46 @@
-var found_locationUrl = "Found_Location.json";
-​
-const map = L.map("map", {
-    center: [30.2672, -97.7431],
-    zoom: 13
-  }); 
-​
-​
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
+const myMap = L.map("map", {
+  center: [30.2672, -97.7431],
+  zoom: 13
+});
+
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
 }).addTo(myMap);
 
-(async function(){
-  data = await d3.json("Found_Location.json");
-  markers = data.filter(d => d["Coord1"]!=null).map(d => {
-      console.log(d["Coord1"])
-      var coord = d["Coord1"].split(",").map(d => +d)
-      return  L.marker(coord, {
-          draggable: false,
-          title: d["Found Location"]
-      }).addTo(myMap);
-  })
-}
-)()
+
+// var found_locationUrl = "Found_Location.json";
+// ​
+// const map = L.map("map", {
+//     center: [30.2672, -97.7431],
+//     zoom: 13
+//   }); 
+// ​
+// ​
+// L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//     maxZoom: 18,
+//     id: "mapbox.streets",
+//     accessToken: API_KEY
+// }).addTo(myMap);
+
+// (async function(){
+//   url='https://raw.githubusercontent.com/busy0312/Project2/master/Found_Location.json'
+//   data = await d3.json(url);
+//   markers = data.filter(d => d["Coord1"]!=null).map(d => {
+//       console.log(d["Coord1"])
+//       var coord = d["Coord1"].split(",").map(d => +d)
+//       return  L.marker(coord, {
+//           draggable: false,
+//           title: d["Found Location"]
+//       }).addTo(myMap);
+//   })
+// }
+// )()
 
 
 
