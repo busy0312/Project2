26e9bc0be753642e4d49912f202a42377477b228
Jamesbarraza <59000118+Jamesbarraza@users.noreply.github.com>
2020-06-11 19:50:00 -0500

    more changes

--- pet_pals/static/js/map.js
+++ pet_pals/static/js/map.js
@@ -1,4 +1,4 @@
-
+console.log("loaded")
 // Create our initial map object
 // Set the longitude, latitude, and the starting zoom level
 const myMap = L.map("map", {
@@ -28,9 +28,9 @@ function makeron(x) {
 }
 
 (async function () {
-    // const url = 'https://raw.githubusercontent.com/busy0312/Project2/master/Found_Location.json'
-    // const data = await d3.json(url);
-    var data = await d3.json("/getData");
+    const url = 'https://raw.githubusercontent.com/busy0312/Project2/master/Found_Location.json'
+    const data = await d3.json(url);
+    // var data = await d3.json("/getData");
     makeron(data)
 })()
 
