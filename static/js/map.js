844dd3f83a79df3a9426a4c5748f65947216a47c
Abby <59398399+busy0312@users.noreply.github.com>
2021-02-03 20:47:42 +0800

    mapbox updated v4 t0 v11

--- static/js/map.js
+++ static/js/map.js
@@ -6,10 +6,18 @@ const myMap = L.map("map", {
     zoom: 11
 });
 
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+// L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//     maxZoom: 17,
+//     id: "mapbox.streets",
+//     accessToken: API_KEY
+// }).addTo(myMap);
+
+// update mapbox
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 17,
-    id: "mapbox.streets",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
 }).addTo(myMap);
 
